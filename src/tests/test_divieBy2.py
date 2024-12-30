44a3c0431200e0614e69caccf3e25efd4273b824
richard-ma <richard.ma.19850509@gmail.com>
2024-12-30 12:05:41 +0800

    add divideBy2

---
+++ src/tests/test_divieBy2.py
@@ -0,0 +1,16 @@
+import unittest
+from divideBy2 import divideBy2
+
+
+class TestDivideBy2(unittest.TestCase):
+    def setUp(self):
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
+    
+    def test_divideBy2(self):
+        decNumber = 233
+        binString = "11101001"
+
+        self.assertEqual(divideBy2(decNumber), binString)
