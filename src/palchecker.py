8a5620494f6f736bc9ea6ac09b34c5ee0d5c47bc
richard-ma <richard.ma.19850509@gmail.com>
2024-12-30 16:12:13 +0800

    add palchecker for Deque

---
+++ src/palchecker.py
@@ -0,0 +1,20 @@
+from basic import Deque
+
+
+# Palindrome-Checker
+def palchecker(aString):
+    chardeque = Deque()
+
+    for ch in aString:
+        chardeque.addRear(ch)
+
+    stillEqual = True
+
+    while chardeque.size() > 1 and stillEqual:
+        first = chardeque.removeFront()
+        last = chardeque.removeRear()
+        if first != last:
+            stillEqual = False
+            break
+    
+    return stillEqual
