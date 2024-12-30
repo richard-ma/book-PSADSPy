809a84bcc7500b8f1263832c647d515155a2c7f3
richard-ma <richard.ma.19850509@gmail.com>
2024-12-30 14:36:20 +0800

    add doMath testcase

--- src/tests/test_infixToPostfix.py
+++ src/tests/test_infixToPostfix.py
@@ -1,5 +1,5 @@
 import unittest
-from infixToPostfix import infixToPostfix
+from infixToPostfix import infixToPostfix, postfixEval, doMath
 
 
 class TestInfixToPostfix(unittest.TestCase):
@@ -9,7 +9,17 @@ class TestInfixToPostfix(unittest.TestCase):
     def tearDown(self):
         return super().tearDown()
     
-    def test_baseConverter(self):
+    def test_infixToPostfix(self):
         infix = "( A + B )"
         postfix = "A B +"
         self.assertEqual(infixToPostfix(infix), postfix)
+
+        self.assertEqual(infixToPostfix("( A + B ) * ( C + D )"), "A B + C D + *")
+        self.assertEqual(infixToPostfix("( A + B ) * C"), "A B + C *")
+        self.assertEqual(infixToPostfix("A + B * C"), "A B C * +")
+
+    def test_doMath(self):
+        self.assertEqual(doMath('+', 1, 2), 3)
+        self.assertEqual(doMath('-', 2, 1), 1)
+        self.assertEqual(doMath('*', 1, 2), 2)
+        self.assertEqual(doMath('/', 4, 2), 2)
