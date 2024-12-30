1d0c3cc340ecc095c877742f0ab1200b318a4720
richard-ma <richard.ma.19850509@gmail.com>
2024-12-30 14:10:36 +0800

    add comments

--- src/infixToPostfix.py
+++ src/infixToPostfix.py
@@ -16,22 +16,22 @@ def infixToPostfix(infixexpr):
     tokenList = infixexpr.split()
 
     for token in tokenList:
-        if token in string.ascii_uppercase:
+        if token in string.ascii_uppercase: # 如果节点是字母，直接添加到后序列表中
             postfixList.append(token)
-        elif token == '(':
+        elif token == '(': # 如果节点是左括号，添加到opStack
             opStack.push(token)
-        elif token == ')':
-            topToken = opStack.pop()
-            while topToken != "(":
-                postfixList.append(topToken)
+        elif token == ')': # 节点是右括号
+            topToken = opStack.pop() # opStack出栈
+            while topToken != "(": # 节点不是匹配的左括号则一直出栈
+                postfixList.append(topToken) # 将所有出栈字符添加到后序列表中
                 topToken = opStack.pop()
-        else:
+        else: # 节点是运算符
             while (not opStack.isEmpty()) and \
-                (prec[opStack.peek()] >= prec[token]):
-                    postfixList.append(opStack.pop())
-            opStack.push(token)
+                (prec[opStack.peek()] >= prec[token]): # opStack不为空且opStack中的栈顶符号优先级高于当前符号
+                    postfixList.append(opStack.pop()) # 将opStack栈顶符号添加到后续队列
+            opStack.push(token) # 将当前符号添加到opStack
     
-    while not opStack.isEmpty():
+    while not opStack.isEmpty(): # 将opStack所有栈中的符号添加到后续列表
         postfixList.append(opStack.pop())
 
     return " ".join(postfixList)
