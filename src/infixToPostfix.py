20974b10988e4c113aef704ac49068696458b596
richard-ma <richard.ma.19850509@gmail.com>
2024-12-30 14:38:45 +0800

    add postfixEval

--- src/infixToPostfix.py
+++ src/infixToPostfix.py
@@ -35,3 +35,29 @@ def infixToPostfix(infixexpr):
         postfixList.append(opStack.pop())
 
     return " ".join(postfixList)
+
+def postfixEval(postfixExpr):
+    operandStack = Stack()
+    
+    tokenList = postfixExpr.split()
+    
+    for token in tokenList:
+        if token in "0123456789":
+            operandStack.push(int(token))
+        else:
+            operand2 = operandStack.pop()
+            operand1 = operandStack.pop()
+            result = doMath(token, operand1, operand2)
+            operandStack.push(result)
+
+    return operandStack.pop()
+
+def doMath(token, op1, op2):
+    if token == '+':
+        return op1 + op2
+    elif token == '-':
+        return op1 - op2
+    elif token == '*':
+        return op1 * op2
+    else:
+        return op1 / op2
