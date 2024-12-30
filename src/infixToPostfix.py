303936a231a2ca692ad1990cf990f21d176ba5e6
richard-ma <richard.ma.19850509@gmail.com>
2024-12-30 13:44:17 +0800

    add infixToPostfix

---
+++ src/infixToPostfix.py
@@ -0,0 +1,37 @@
+from basic import Stack
+import string
+
+
+def infixToPostfix(infixexpr):
+    prec = dict()
+    prec['*'] = 3
+    prec['/'] = 3
+    prec['+'] = 2
+    prec['-'] = 2
+    prec['('] = 1
+
+    opStack = Stack()
+    postfixList = list()
+    
+    tokenList = infixexpr.split()
+
+    for token in tokenList:
+        if token in string.ascii_uppercase:
+            postfixList.append(token)
+        elif token == '(':
+            opStack.push(token)
+        elif token == ')':
+            topToken = opStack.pop()
+            while topToken != "(":
+                postfixList.append(topToken)
+                topToken = opStack.pop()
+        else:
+            while (not opStack.isEmpty()) and \
+                (prec[opStack.peek()] >= prec[token]):
+                    postfixList.append(opStack.pop())
+            opStack.push(token)
+    
+    while not opStack.isEmpty():
+        postfixList.append(opStack.pop())
+
+    return " ".join(postfixList)
