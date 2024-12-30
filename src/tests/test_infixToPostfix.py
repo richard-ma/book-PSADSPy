a2a78b841e5e583b75f221bc9f9b31865822a472
richard-ma <richard.ma.19850509@gmail.com>
2024-12-30 15:01:27 +0800

    add postfixEval testcase

--- src/tests/test_infixToPostfix.py
+++ src/tests/test_infixToPostfix.py
@@ -23,3 +23,14 @@ class TestInfixToPostfix(unittest.TestCase):
         self.assertEqual(doMath('-', 2, 1), 1)
         self.assertEqual(doMath('*', 1, 2), 2)
         self.assertEqual(doMath('/', 4, 2), 2)
+
+    def test_postfixEval(self):
+        exprs = [
+            ("1 + 2", 3),
+            ("2 * 2", 4),
+            ("1 + 2 * 3", 7),
+            ("( 1 + 2 ) * ( 3 + 4 )", 21),
+        ]
+
+        for expr, result in exprs:
+            self.assertEqual(postfixEval(infixToPostfix(expr)), result)
