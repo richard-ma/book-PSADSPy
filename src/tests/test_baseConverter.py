b531e7d8216353c51bf2098a00a1fd1109f26e57
richard-ma <richard.ma.19850509@gmail.com>
2024-12-30 13:18:33 +0800

    add baseConverter

---
+++ src/tests/test_baseConverter.py
@@ -0,0 +1,20 @@
+import unittest
+from baseConverter import baseConverter
+
+
+class TestBaseConverter(unittest.TestCase):
+    def setUp(self):
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
+    
+    def test_baseConverter(self):
+        decNumber = 233
+        binString = "11101001"
+        octString = "351"
+        hexString = "E9"
+
+        self.assertEqual(baseConverter(decNumber, 2), binString)
+        self.assertEqual(baseConverter(decNumber, 8), octString)
+        self.assertEqual(baseConverter(decNumber, 16), hexString)
