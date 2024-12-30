b57500197f9edc993c00c6ee39ef93dd48c495e5
richard-ma <richard.ma.19850509@gmail.com>
2024-12-30 21:39:47 +0800

    add hanoi

---
+++ pythonds/hanoi.py
@@ -0,0 +1,11 @@
+def moveTower(height, fromPole, toPole, withPole):
+    if height >= 1:
+        moveTower(height-1, fromPole, withPole, toPole)
+        moveDisk(fromPole, toPole)
+        moveTower(height-1, withPole, toPole, fromPole)
+        
+def moveDisk(fp, tp):
+    print("moving disk from %d to %d." % (fp, tp))
+
+
+moveTower(3, 1, 3, 2)
