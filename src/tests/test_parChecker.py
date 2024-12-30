3ee7d4a0458ad988c095e74180c29834fbd959a0
richard-ma <richard.ma.19850509@gmail.com>
2024-12-30 11:59:32 +0800

    add parChecker

---
+++ src/tests/test_parChecker.py
@@ -0,0 +1,19 @@
+import unittest
+from parChecker import parChecker
+
+
+class TestParChecker(unittest.TestCase):
+    def setUp(self):
+        return super().setUp()
+    
+    def tearDown(self):
+        return super().tearDown()
+    
+    def test_parChecker(self):
+        self.assertTrue(parChecker("{{([][])}()}"))
+        self.assertTrue(parChecker("[[{{(())}}]]"))
+        self.assertTrue(parChecker("[][][](){}"))
+                
+        self.assertFalse(parChecker("([)]"))
+        self.assertFalse(parChecker("((()]))"))
+        self.assertFalse(parChecker("[{()]"))
