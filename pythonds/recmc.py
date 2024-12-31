eb49a74d0b2c7257d4f7f694d4b708b1d8308316
richard-ma <richard.ma.19850509@gmail.com>
2024-12-31 09:27:56 +0800

    add recDC

--- pythonds/recmc.py
+++ pythonds/recmc.py
@@ -9,5 +9,23 @@ def recMC(coinValueList, change):
                 minCoins = numCoins
     return minCoins
 
-result = recMC([1, 5, 10, 25], 6)
+def recDC(coinValueList, change, knowResults):
+    minCoins = change
+    if change in coinValueList:
+        knowResults[change] = 1
+        return 1
+    elif knowResults[change] > 0:
+        return knowResults[change]
+    else:
+        for i in [c for c in coinValueList if c <= change]:
+            numCoins = 1 + recDC(coinValueList, change-i, knowResults)
+            if numCoins < minCoins:
+                minCoins = numCoins
+                knowResults[change] = minCoins
+    return minCoins
+        
+
+result = recMC([1, 5, 10, 25], 63)
+print(result)
+result = recDC([1, 5, 10, 25], 63, [0]*64)
 print(result)
