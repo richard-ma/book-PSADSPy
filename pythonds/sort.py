a51beebbb8c2288f0196f9b24049d6f1e68dc8ad
richard-ma <richard.ma.19850509@gmail.com>
2025-01-02 11:55:00 +0800

    add merge sort

--- pythonds/sort.py
+++ pythonds/sort.py
@@ -70,3 +70,34 @@ def gapInsertionSort(alist, start, gap):
             position = position - gap
 
         alist[position] = currentvalue
+
+def mergeSort(alist):
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        
+        i, j, k = 0, 0, 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i += 1
+            else:
+                alist[k] = righthalf[j]
+                j += 1
+            k += 1
+        
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i += 1
+            k += 1
+        
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j += 1
+            k += 1
+        
+        return alist
