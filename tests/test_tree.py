acf964ff89eb5db109faa95182840354ddddc0c8
richard-ma <richard.ma.19850509@gmail.com>
2025-01-02 15:21:56 +0800

    add Binary tree class testcase

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -8,3 +8,25 @@ class TestTree(unittest.TestCase):
     
     def tearDown(self):
         return super().tearDown()
+
+    def test_getLeftChild(self):
+        r = BinaryTree(3)
+        r.insertLeft(4)
+        node = r.getLeftChild()
+        self.assertEqual(4, node.getRootVal())
+
+    def test_getRightChild(self):
+        r = BinaryTree(3)
+        r.insertRight(4)
+        node = r.getRightChild()
+        self.assertEqual(4, node.getRootVal())
+
+    def test_get_and_set_rootval(self):
+        r = BinaryTree('a')
+        rootval = r.getRootVal()
+        self.assertEqual('a', rootval)
+
+        r.setRootVal('b')
+        rootval = r.getRootVal()
+        self.assertEqual('b', rootval)
+        
