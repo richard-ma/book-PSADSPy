39e7f03f154a4caab008680a78d7ae6da05805cd
richard-ma <richard.ma.19850509@gmail.com>
2025-01-02 13:19:48 +0800

    add tree test file

---
+++ tests/test_tree.py
@@ -0,0 +1,10 @@
+import unittest
+from pythonds.tree import *
+
+
+class TestTree(unittest.TestCase):
+    def setUp(self):
+        return super().setUp()
+    
+    def tearDown(self):
+        return super().tearDown()
